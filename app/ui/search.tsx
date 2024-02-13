91c007e45e3e358e490867aac4ad73756f84052a
Cherrypick14 <cherrypicklet@gmail.com>
2024-02-13 22:45:50 +0300

    updating the url with users search data

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,11 +1,13 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams } from 'next/navigation';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { log } from 'console';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
+  const pathname =  usePathname();
+  const {replace} = useRouter();
 
    function handleSearch(term: string){
 
@@ -15,6 +17,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       }else{
         params.delete('query');
       }
+      replace(`{${pathname}}?${params.toString()}`);
    }
   return (
     <div className="relative flex flex-1 flex-shrink-0">
