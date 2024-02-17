1256a7d19112c5a3e93bee3c237932d133c5b08d
Cherrypick14 <cherrypicklet@gmail.com>
2024-02-17 23:49:01 +0300

    adding pagination to our invoices

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -5,8 +5,9 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
+import { fetchInvoicesPages } from '@/app/lib/data';
 
-export default function Page({searchParams,}: {
+export default async function Page({searchParams,}: {
    searchParams?: {
     query?:string
     page?: string
@@ -14,7 +15,9 @@ export default function Page({searchParams,}: {
 }){
    const query = searchParams?.query || '' ;
    const currentPage = Number(searchParams?.page) || 1 ;
-   
+
+   const totalPages =  await fetchInvoicesPages(query);
+
     return (
         <div className="w-full">
       <div className="flex w-full items-center justify-between">
