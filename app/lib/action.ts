49a5a6ff440edeec1d1d9ad150bc90f9df070f04
Cherrypick14 <cherrypicklet@gmail.com>
2024-02-29 00:08:45 +0300

    finish writing updateinvoice server action

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -36,5 +36,22 @@ export async function createInvoice(formData: FormData) {
     
 }
 
-const updateInvoice = FormSchema.omit({id: true, date: true})
+const UpdateInvoice = FormSchema.omit({id: true, date: true})
 
+export async function updateInvoice(id: string, formData: FormData){
+    const{customerId, amount, status}= UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status')
+    });
+    
+    const amountInCents = amount * 100;
+
+    await sql `
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount= ${amountInCents}, ${status}
+    WHERE id=${id}`;
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
