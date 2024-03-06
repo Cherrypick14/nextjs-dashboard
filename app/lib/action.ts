c0cdff574ae2d27193d3b45a7845871cbe520b7a
Cherrypick14 <cherrypicklet@gmail.com>
2024-03-06 23:53:31 +0300

    finish validating fields

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -45,10 +45,11 @@ export async function createInvoice(prevState: State, formData: FormData) {
             message: 'Missing Fields. Failed to Create Invoice.'
         };      
     }
-    
-
+    const{customerId, amount, status} = validateFields.data;
     const amountInCents = amount * 100 ;
     const date =  new Date().toISOString().split('T')[0]
+
+    // inserting data into the db
     try {
         await sql`
         INSERT INTO invoices (customer_id, amount, status, date)
